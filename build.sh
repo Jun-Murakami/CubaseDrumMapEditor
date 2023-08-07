e7ae1c4f27c36ca0f96f62e277550fc6ca9dd16d
Jun Murakami <contact@bucketrelay.com>
2023-08-07 23:37:58 +0900

    forV1.0

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ mkdir -p CubaseDrumMapEditor.app/Contents/MacOS
 cp -r bin/Release/net6.0/osx-x64/publish/* CubaseDrumMapEditor.app/Contents/MacOS
 cp Info.plist CubaseDrumMapEditor.app/Contents/
 mkdir CubaseDrumMapEditor.app/Contents/Resources
-cp Assets/avalonia-logo.ico.icns CubaseDrumMapEditor.app/Contents/Resources
+cp Assets/avalonia-logo.icns CubaseDrumMapEditor.app/Contents/Resources
