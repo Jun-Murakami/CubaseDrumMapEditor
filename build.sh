71e60ea9882146db3e658f3eb36e4c1e5a5e08d9
Jun Murakami <contact@bucketrelay.com>
2023-08-07 23:21:09 +0900

    forV1.0

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dotnet restore -r osx-x64
+dotnet publish -c Release --self-contained -p:PublishSingleFile=false --runtime osx-x64
+mkdir -p CubaseDrumMapEditor.app/Contents/MacOS
+cp -r bin/Release/net6.0/osx-x64/publish/* CubaseDrumMapEditor.app/Contents/MacOS
+cp Info.plist CubaseDrumMapEditor.app/Contents/
+mkdir CubaseDrumMapEditor.app/Contents/Resources
+cp Assets/avalonia-logo.ico.icns CubaseDrumMapEditor.app/Contents/Resources
